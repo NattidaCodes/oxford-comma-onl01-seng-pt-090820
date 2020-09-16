9a3c25040f25d7cfc43e37d4e85a01014a98c5a0
Nattida Wong <nattidac.wong@gmail.com>
2020-09-16 13:59:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,16 @@
+# def oxford_comma(array)
+
+# end
+
+
 def oxford_comma(array)
-  if array.length > 3
+  if array.size == 1
+    array.join(", ")
+  elsif
+    array.size == 2
+    array.join(" and ")
+  else
+    array[-1] = "and " + array[-1]
+    array.join(", ")
   end
 end
